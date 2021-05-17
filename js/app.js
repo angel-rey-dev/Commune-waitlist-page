0f55f0c91269c1ebde58c1b47113554e06e9784b
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-05-17 06:25:41 -0400

    Update

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
+// Form validation
+const form = document.querySelector(".main-content__form");
+let email = document.getElementById("email");
+let emailPattern = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+
+form.addEventListener("submit", (e) => {
+  if (!email.value.match(emailPattern) || email.value === "") {
+    e.preventDefault();
+    email.classList.add("invalid");
+    email.parentElement.nextElementSibling.classList.add("active-warning");
+  } else {
+    email.classList.remove("invalid");
+    email.classList.add("valid");
+    email.parentElement.nextElementSibling.classList.remove("active-warning");
+  }
+});
+
+// Hamburger button
 const burgerBtn = document.querySelector(".main-header__hamburger-btn");
 
 burgerBtn.addEventListener("click", () => {
